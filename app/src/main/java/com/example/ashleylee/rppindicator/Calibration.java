0244a989905fcb4403dbe7067b99dcf53753743c
ajlee19 <ashleyjlee19@gmail.com>
2017-07-28 17:02:31 +0900

    Update Calibration.java

--- app/src/main/java/com/example/ashleylee/rppindicator/Calibration.java
+++ app/src/main/java/com/example/ashleylee/rppindicator/Calibration.java
@@ -14,16 +14,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class Calibration extends AppCompatActivity {
+import sds.mhs.android.BPAlgorithm;
+import sds.mhs.android.BpEventListener;
+
+public class Calibration extends AppCompatActivity implements BpEventListener {
 
     private SensorManager sensorManager;
     private Sensor hrm;
-    private Button calButton;
-    private ProgressBar spinner;
     private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private Button calButton;
+    private EditText sbpRef;
+    private EditText dbpRef;
+    private EditText hrRef;
+    private ProgressBar spinner;
+
+    private BPAlgorithm bp;
+    private BPAlgorithm bpAlgorithm;
+    private double mFeat1, mFeat2, mFeat3, mFeat4;
+    private int mSbpOffset, mDbpOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,9 @@ public class Calibration extends AppCompatActivity {
         setContentView(R.layout.activity_calibration);
 
         calButton = (Button) findViewById(R.id.cal_button);
+        sbpRef = (EditText) findViewById(R.id.sbp_input);
+        dbpRef = (EditText) findViewById(R.id.dbp_input);
+        hrRef = (EditText) findViewById(R.id.hr_input);
 
         // Checking permission
         if (checkPermission()){
@@ -41,6 +57,9 @@ public class Calibration extends AppCompatActivity {
 
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         hrm = sensorManager.getDefaultSensor(65584);
+
+        bpAlgorithm = new BPAlgorithm(this);
+        bpAlgorithm.setBpChangedListener(this);
 
         spinner = (ProgressBar)findViewById(R.id.progressBarCal);
         spinner.setVisibility(View.GONE);
@@ -83,17 +102,54 @@ public class Calibration extends AppCompatActivity {
 
     public void calibrate(View view){
         if (calButton.getText().toString().equals("Done")){
-            //start next activity
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            continueToMain();
         }
-
-        //spinner.setVisibility(View.VISIBLE);
+        spinner.setVisibility(View.VISIBLE);
+        bpAlgorithm.resetFeature();
+        bpAlgorithm.resetReq();
         turnOnSensor();
-        // BP BEASURE
-        //spinner.setVisibility(View.GONE);
         turnOffSensor();
+        spinner.setVisibility(View.GONE);
         calButton.setText(R.string.cal_button_done);
+    }
+
+    public void continueToMain(){
+        double[] features = new double[4];
+        features[0] = mFeat1;
+        features[1] = mFeat2;
+        features[2] = mFeat3;
+        features[3] = mFeat4;
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("sbpOffset", mSbpOffset);
+        intent.putExtra("dbpOffset", mDbpOffset);
+        intent.putExtra("features", features);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBpChanged(short sbp, short dbp, short hr) {
+        String sbpRefStr = sbpRef.getText().toString();
+        String dbpRefStr = dbpRef.getText().toString();
+        String hrRefStr = hrRef.getText().toString();
+
+        if (calButton.getText().equals("DONE")) {
+            if (!sbpRefStr.isEmpty() && !dbpRefStr.isEmpty()) {
+                mSbpOffset = Integer.valueOf(sbpRefStr) - sbp;
+                mDbpOffset = Integer.valueOf(dbpRefStr) - dbp;
+            } else {
+                mSbpOffset = 0;
+                mDbpOffset = 0;
+            }
+        }
+    }
+
+    @Override
+    public void onCalDone(double feat1, double feat2, double feat3, double feat4) {
+        mFeat1 = feat1;
+        mFeat2 = feat2;
+        mFeat3 = feat3;
+        mFeat4 = feat4;
+        Log.d("test", String.format("Feat: %.4f, %.4f, %.4f, %.4f", mFeat1, mFeat2, mFeat3, mFeat4));
     }
 
     private void turnOnSensor(){
