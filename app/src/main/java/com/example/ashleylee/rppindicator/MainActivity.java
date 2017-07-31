6331204cc9dedfda194685b1483a4332b0e644fe
ajlee19 <ashleyjlee19@gmail.com>
2017-07-31 18:07:36 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/ashleylee/rppindicator/MainActivity.java
+++ app/src/main/java/com/example/ashleylee/rppindicator/MainActivity.java
@@ -1,4 +1,4 @@
-package ashleylee.rpp_demo;
+package  com.samsung.rpp_demo;
 
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -6,49 +6,36 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-
+import com.samsung.sds.mhs.android.BPAlgorithm;
+import com.samsung.sds.mhs.android.BpEventListener;
 import java.util.ArrayList;
-
-import sds.mhs.android.BPAlgorithm;
-import sds.mhs.android.BpEventListener;
+import samsung.rpp_demo.R;
 
 public class MainActivity extends AppCompatActivity implements BpEventListener {
 
-    private BPAlgorithm bp;
     private BPAlgorithm bpAlgorithm;
     private int sbpOffset;
     private double mFeat1, mFeat2, mFeat3, mFeat4;
 
-    private TextView hrText, rppText, shortSum;
+    private TextView hrText, rppText;
     private Button measureBtn;
-    private int aSbp, aDbp;
+    private int aSbp;
     private int rppMeasured;
     private ArrayList<Integer> hrData, rppData;
+    double[] features;
 
     private SensorManager sensorManager;
     private Sensor hrm;
 
     private GraphView graph;
     private LineGraphSeries<DataPoint> series;
-    private final Handler mHandler = new Handler();
-    private Runnable mTimer1;
-    private Runnable mTimer2;
-    private LineGraphSeries<DataPoint> mSeries1;
-    private LineGraphSeries<DataPoint> mSeries2;
-    private double graph2LastXValue = 5d;
-
-    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,57 +44,56 @@ public class MainActivity extends AppCompatActivity implements BpEventListener {
 
         Intent intent = getIntent();
         sbpOffset = intent.getIntExtra("sbpOffset", 0);
-        double[] features = intent.getDoubleArrayExtra("features");
-        onCalDone(features[0], features[1], features[2], features[3]);
+        features = intent.getDoubleArrayExtra("features");
+
         hrText = (TextView) findViewById(R.id.heart_rate);
         rppText = (TextView) findViewById(R.id.rpp);
-        shortSum = (TextView) findViewById(R.id.shortSum);
-        measureBtn = (Button) findViewById(R.id.btn_measure);
-        measureBtn.setText(R.string.measure_button);
+        measureBtn = (Button) findViewById(R.id.measure_button);
 
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         hrm = sensorManager.getDefaultSensor(65584); // WHAT IS THE SENSOR TYPE
+        hrData = new ArrayList<>();
+        rppData = new ArrayList<>();
 
         graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>(new DataPoint[] {});
-//        Viewport.viewport = graph.getViewport();
-//        viewport.setYAxisBoundsManual(true);
-//        viewport.setMinY(5);
-//        viewport.setMaxY(25);
-//        viewport.setScrollable(true);
-
         graph.addSeries(series);
+
         bpAlgorithm = new BPAlgorithm(this);
         bpAlgorithm.setBpChangedListener(this);
+        bpAlgorithm.setFeature(features[0], features[1], features[2], features[3]);
     }
 
     public void measureRpp(View view){
-        measureBtn.setText("Measuring");
-        hrData = new ArrayList<>();
-        rppData = new ArrayList<>();
-        bpAlgorithm.resetReq();
-        turnOnSensor();
-        // do nothing?
-        turnOffSensor();
-        measureBtn.setText(R.string.measure_button);
+        if (measureBtn.getText().toString().equals("Measuring")){
+            turnOffSensor();
+            measureBtn.setText("Measure RPP");
+            hrText.setText("");
+            rppText.setText("");
+        } else{
+            measureBtn.setText("Measuring");
+            hrText.setText("--");
+            rppText.setText("--");
+            hrData = new ArrayList<>();
+            rppData = new ArrayList<>();
+            bpAlgorithm.resetReq();
+            turnOnSensor();
+        }
     }
 
     SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
-        public void onSensorChanged(SensorEvent event) {}
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case 65584:
+                    bpAlgorithm.pushData((int)event.values[2]);
+//                    Log.d("test","value : "+ event.values[2]);
+                    break;
+            }
+        }
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
     };
-
-    private void turnOnSensor(){
-        sensorManager.registerListener(sensorEventListener, hrm, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    private void turnOffSensor(){
-        if (sensorManager != null){
-            sensorManager.unregisterListener(sensorEventListener);
-        }
-    }
 
     @Override
     public void onBpChanged(short sbp, short dbp, short hr) {
@@ -116,15 +102,6 @@ public class MainActivity extends AppCompatActivity implements BpEventListener {
 
         rppText.setText(String.valueOf(rppMeasured));
         hrText.setText(String.valueOf(hr));
-        if (rppMeasured <= 12){
-            shortSum.setText("Normal");
-        }else if (rppMeasured <= 17){
-            shortSum.setText("At Risk");
-        }else if (rppMeasured <= 21){
-            shortSum.setText("Danger");
-        }else{
-            shortSum.setText("Very Danger");
-        }
 
         hrData.add((int) hr);
         rppData.add(rppMeasured);
@@ -136,13 +113,35 @@ public class MainActivity extends AppCompatActivity implements BpEventListener {
         mFeat2 = feat2;
         mFeat3 = feat3;
         mFeat4 = feat4;
-        Log.d("test", String.format("Feat: %.4f, %.4f, %.4f, %.4f", mFeat1, mFeat2, mFeat3, mFeat4));
+        //Log.d("test", String.format("Feat: %.4f, %.4f, %.4f, %.4f", mFeat1, mFeat2, mFeat3, mFeat4));
+    }
+
+    private void turnOnSensor(){
+        sensorManager.registerListener(sensorEventListener, hrm, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void turnOffSensor(){
+        if (sensorManager != null){
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+    }
+
+    private int calculateAverage(ArrayList<Integer> data){
+        int tot = 0;
+        for (Integer i: data){tot += i;}
+        return Math.round(tot/data.size());
     }
 
     public void viewRPPAnalysis(View view) {
+        int hrAvg = calculateAverage(hrData);
+        int rppAvg = calculateAverage(rppData);
+        turnOffSensor();
         Intent intent = new Intent(this, ExerciseSuggestion.class);
-        intent.putExtra("hrData", hrData);
-        intent.putExtra("rppData", rppData);
+        intent.putExtra("hrAvg", hrAvg);
+        intent.putExtra("rppAvg", rppAvg);
+
+        intent.putExtra("features", features);
+        intent.putExtra("sbpOffset", sbpOffset);
         startActivity(intent);
     }
 }
