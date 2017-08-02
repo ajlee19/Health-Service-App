9ea182075e1039d3e088ed4112ad65339fcbc69d
ajlee19 <ashleyjlee19@gmail.com>
2017-08-02 18:29:08 +0900

    Create Additional.java

---
+++ app/src/main/java/com/example/ashleylee/rppindicator/Additional.java
@@ -0,0 +1,18 @@
+package com.samsung.rpp_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import samsung.rpp_demo.R;
+
+public class Additional extends AppCompatActivity {
+
+    private int sbpOffset;
+    private double[] features;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional);
+    }
+}
