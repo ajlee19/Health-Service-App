2af97e1f2ed485471992fbbf1b3802346c55586d
ajlee19 <ashleyjlee19@gmail.com>
2017-08-04 18:14:46 +0900

    Create views.py

---
+++ other/visualizemap/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.template import Context, Template, loader
+
+def index(request):
+    template = loader.get_template('_content.html')
+    return HttpResponse(template.render())
+
+def intro(request):
+    template = loader.get_template('_intro.html')
+    return HttpResponse(template.render())
+
+def content(request):
+    template = loader.get_template('_content.html')
+    return HttpResponse(template.render())
+
+def contact(request):
+    template = loader.get_template('_contact.html')
+    return HttpResponse(template.render())
+
+def main(request):
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
